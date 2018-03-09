9211a012b359fa5bc1b74b8fb9abed59a0a69626
arun <arunrs123@gmail.com>
2018-03-09 17:14:36 +0530

    asd

---
+++ src/app/shopping/addproducts/addproducts.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+import { AfterViewInit, AfterContentInit, OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ActivatedRoute } from '@angular/router';
+import { Params } from '@angular/router/src/shared';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-addproducts',
+  templateUrl: './addproducts.component.html',
+  styleUrls: ['./addproducts.component.css']
+})
+export class AddproductsComponent implements OnInit {
+  additemform:FormGroup;
+  editmode:boolean;
+  edititem:Product;
+  constructor(private serv:ProductService,private route: ActivatedRoute,private router:Router) {
+
+    
+   }
+
+  ngOnInit() {
+    this.oninitform();
+    this.route.queryParams.subscribe((params: Params) => {
+          let editmode = params['id'];
+        
+          if(editmode){
+              this.editmode = true;
+              this.edititem = this.serv.geteditproduct(editmode);
+              this.additemform.setValue({
+                'productname': this.edititem.productname,
+                'productcost' : this.edititem.productcost,
+                'productimage' : this.edititem.productimage,
+                'detail' : this.edititem.detail,
+                'productcolor': this.edititem.productcolor,
+              })
+            }
+            else{
+              this.editmode = false;
+              this.additemform.reset();
+            }
+        });
+  
+  }
+
+
+
+  oninitform(){
+    this.additemform = new FormGroup({
+      'productname': new FormControl('',Validators.required),
+       'productcost' : new FormControl('',Validators.required),
+       'productimage' : new FormControl('',Validators.required),
+       'detail' : new FormControl('',Validators.required),
+       'productcolor': new FormControl('',Validators.required),
+    });
+    
+  }
+
+  onsubmit(){
+    if(this.editmode){
+      this. Updateproduct();
+    }
+    else{
+      this.serv.addproducts(this.additemform.value);
+      this.additemform.reset();
+    }
+    
+  }
+
+  Updateproduct(){
+    alert("updated");
+    this.router.navigate(['/shopping/viewproducts'])
+  }
+
+
+ 
+  
+
+
+
+}
